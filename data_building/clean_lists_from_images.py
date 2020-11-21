4c05bdb50760843fca8735742abf5926d3f14525
vivianmahr <sendak.vivian.mahr@gmail.com>
2020-11-21 11:35:37 -0800

    more dupes removed because clustering them made duplicate spotting easier

--- data_building/clean_lists_from_images.py
+++ data_building/clean_lists_from_images.py
@@ -4,33 +4,56 @@ run this to update the corresponding list of links to match. That way, if run on
 another computer, you can just use the recorded list of links. 
 """
 import os
-name="utilities"
-check_list = "./lists_of_links/{}.txt".format(name)
-files = "./images/{}".format(name)
-#output = check_list.split(r"/")[-1]
-output = check_list
+# leave out demoscene, it has no links
+names=[
+    'adult',
+    'alcohol',
+    'convenience_store',
+    'dental_pharmaceutical',
+    'electronics', 'esports',
+    'food_drink',
+    'gaming',
+    'gas',
+    'media_and_communication',
+    'misc',
+    'money',
+    'restaurant',
+    'software',
+    'sports',
+    'sports_clothing',
+    'store',
+    'utilities'
+]
+
 
 def remove_extension(f):
     return ".".join(f.split(".")[:-1])
+for name in names:
+    print(name)
+    check_list = "./lists_of_links/{}.txt".format(name)
+    files = "./images/{}".format(name)
+    #output = check_list.split(r"/")[-1]
+    output = check_list
 
-all_images = [remove_extension(f) for f in os.listdir(files)]
-total_images = len(all_images)
+    all_images = [remove_extension(f) for f in os.listdir(files)]
+    total_images = len(all_images)
 
-with open(check_list) as f:
-    full_urls = [x.strip() for x in f.readlines()]
+    with open(check_list) as f:
+        full_urls = [x.strip() for x in f.readlines()]
 
-new = []
-for url in full_urls:
-    file_name = url.split(r"/")[-1].lower()
-    if file_name in all_images:
-        new.append(url)
-        all_images.remove(file_name)
+    new = []
+    for url in full_urls:
+        file_name = url.split(r"/")[-1].lower()
+        if file_name in all_images:
+            new.append(url)
+            all_images.remove(file_name)
 
-with open(output, "w") as f:
-    f.write("\n".join(new))
-    
-print("urls saved: ", len(new))
-print("total images:", total_images)
-print("leftover images :")
-for i in all_images:
-    print("\t", i)
+    with open(output, "w") as f:
+        f.write("\n".join(new))
+        
+    print("urls saved: ", len(new))
+    print("total images:", total_images)
+    print("leftover images :")
+    for i in all_images:
+        print("\t", i)
+
