7917ae42baaaacaae0f8abc4c55f7d1d7b4ea963
sendak <sendak.vivian.mahr@gmail.com>
2021-04-01 00:20:49 -0700

    list of links updated

--- data_building/clean_lists_from_images.py
+++ data_building/clean_lists_from_images.py
@@ -3,8 +3,10 @@ After manually going through images and removing non-logo images,
 run this to update the corresponding list of links to match. That way, if run on
 another computer, you can just use the recorded list of links. 
 """
+
 import os
 # leave out demoscene, it has no links
+"""
 names=[
     'adult',
     'alcohol',
@@ -24,10 +26,36 @@ names=[
     'store',
     'utilities'
 ]
+"""
+g_play_names = os.listdir("./lists_of_links/google_play")
+
 
 
 def remove_extension(f):
     return ".".join(f.split(".")[:-1])
+for name in g_play_names:
+    if name == "cat_list.txt": continue
+    check_list = "./lists_of_links/google_play/{}".format(name)
+    files = "./images/g_play/{}".format(remove_extension(name))
+    output = check_list
+
+    all_images = [remove_extension(f) for f in os.listdir(files)]
+    print(all_images)
+    total_images = len(all_images)
+
+    with open(check_list) as f:
+        full_urls = [x.strip().split("\t") for x in f.readlines()]
+
+    new = []
+    for f_url in full_urls:
+        file_name = f_url[0]
+        url = f_url[1]
+        if file_name in all_images:
+            new.append("\t".join(f_url))
+            all_images.remove(file_name)
+    with open(output, "w") as f:
+        f.write("\n".join(new))
+"""
 for name in names:
     print(name)
     check_list = "./lists_of_links/{}.txt".format(name)
@@ -57,3 +85,4 @@ for name in names:
     for i in all_images:
         print("\t", i)
 
+"""
