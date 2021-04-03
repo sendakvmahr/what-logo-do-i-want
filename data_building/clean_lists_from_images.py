124dc6b2cfddba73737525d33545367705c75b2d
sendak <sendak.vivian.mahr@gmail.com>
2021-04-03 01:12:00 -0700

    added the organized csvs, cleaned up some bad images

--- data_building/clean_lists_from_images.py
+++ data_building/clean_lists_from_images.py
@@ -6,83 +6,45 @@ another computer, you can just use the recorded list of links.
 
 import os
 # leave out demoscene, it has no links
-"""
-names=[
-    'adult',
-    'alcohol',
-    'convenience_store',
-    'dental_pharmaceutical',
-    'electronics', 'esports',
-    'food_drink',
-    'gaming',
-    'gas',
-    'media_and_communication',
-    'misc',
-    'money',
-    'restaurant',
-    'software',
-    'sports',
-    'sports_clothing',
-    'store',
-    'utilities'
-]
-"""
-g_play_names = os.listdir("./lists_of_links/google_play")
 
+wiki_names=sorted(os.listdir("./lists_of_links/wikipedia"))
 
+g_play_names = sorted(os.listdir("./lists_of_links/g_play"))
+g_play_names.remove("cat_list.txt")
 
 def remove_extension(f):
     return ".".join(f.split(".")[:-1])
-for name in g_play_names:
-    if name == "cat_list.txt": continue
-    check_list = "./lists_of_links/google_play/{}".format(name)
-    files = "./images/g_play/{}".format(remove_extension(name))
-    output = check_list
 
-    all_images = [remove_extension(f) for f in os.listdir(files)]
-    print(all_images)
-    total_images = len(all_images)
+#for source in ["wikipedia", "g_play"]:
+for source in ["wikipedia"]:
+    names = wiki_names if source == "wikipedia" else g_play_names
+    for name in names:
+        #print("========== {} : {}".format(source, name))
+        check_list = "./lists_of_links/{}/{}".format(source, name)
+        files = "./images/{}/{}".format(source, remove_extension(name))
+        output = check_list
 
-    with open(check_list) as f:
-        full_urls = [x.strip().split("\t") for x in f.readlines()]
+        all_images = [remove_extension(f) for f in os.listdir(files)]
+        total_images = len(all_images)
+        with open(check_list) as f:
+            full_urls = [x.strip().split("\t") for x in f.readlines()]
 
-    new = []
-    for f_url in full_urls:
-        file_name = f_url[0]
-        url = f_url[1]
-        if file_name in all_images:
-            new.append("\t".join(f_url))
-            all_images.remove(file_name)
-    with open(output, "w") as f:
-        f.write("\n".join(new))
-"""
-for name in names:
-    print(name)
-    check_list = "./lists_of_links/{}.txt".format(name)
-    files = "./images/{}".format(name)
-    #output = check_list.split(r"/")[-1]
-    output = check_list
+        new = []
+        missing = []
+        for f_url in full_urls:
+            file_name = f_url[0]
+            url = f_url[1]
+            if file_name in all_images:
+                new.append("\t".join(f_url))
+                all_images.remove(file_name)
+            else:
+                missing.append("{} {} {}".format(source, remove_extension(name), file_name))
 
-    all_images = [remove_extension(f) for f in os.listdir(files)]
-    total_images = len(all_images)
+        with open(output, "w") as f:
+            f.write("\n".join(new))
 
-    with open(check_list) as f:
-        full_urls = [x.strip() for x in f.readlines()]
-
-    new = []
-    for url in full_urls:
-        file_name = url.split(r"/")[-1].lower()
-        if file_name in all_images:
-            new.append(url)
-            all_images.remove(file_name)
-
-    with open(output, "w") as f:
-        f.write("\n".join(new))
-        
-    print("urls saved: ", len(new))
-    print("total images:", total_images)
-    print("leftover images :")
-    for i in all_images:
-        print("\t", i)
-
-"""
+        if len(new) != total_images or len(missing) != 0:
+            print("========== {} : {}".format(source, name))
+            print("urls saved: {} / {}".format(len(new), len(full_urls)))
+            print("total images:", total_images)
+            print("delted from csv: {}".format(missing))
